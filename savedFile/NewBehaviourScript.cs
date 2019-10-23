f5b94f197ae5582152e15c9862276440998b3d6f
SylvainSenechal <senechalsylvain@hotmail.com>
2019-10-23 22:00:27 +0200

    tensorflow code utlity

---
+++ savedFile/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+  void SetGameObjectPosition(float data) {
+    Debug.Log("OKKKKPOURKOI");
+
+    var position = data;
+    Debug.Log(position);
+    Debug.Log(position);
+    Debug.Log(position);
+    this.transform.position = new Vector3(data*3 , 0, 0);
+    Debug.Log(position);
+  }
+
+  void SetGameObjectPosition2(string data) {
+    Debug.Log("OKKKKPOURKOI");
+    Debug.Log(data);
+    var position = JsonUtility.FromJson<Vector3>(data);
+    Debug.Log(position);
+    Debug.Log(position);
+    Debug.Log(position);
+    this.transform.position = position;
+    Debug.Log(position);
+  }
+
+  void SetLeftWrist(string data) {
+    Debug.Log("Setting left Wrist");
+    Debug.Log(data);
+    var position = JsonUtility.FromJson<Vector3>(data);
+    Debug.Log(position);
+    Debug.Log(position);
+    Debug.Log(position);
+    this.transform.position = position;
+    Debug.Log(position);
+  }
+}

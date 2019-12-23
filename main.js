25e9a51ee068ac4e1c8ae41991afa87ec38829af
SylvainSenechal <senechalsylvain@hotmail.com>
2019-12-23 23:37:43 +0100

    added canvas position drawing

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 import init from './sceneSetup.js'
 import {Wall} from './object.js'
 import {initControls, cameraMover} from './cameraControls.js'
-// import {estimate, loadNet} from './poseDetector.js'
+import {loadPoseNet, startCamera, estimatePoseOnImage} from './poseDetector.js'
 
 let {scene, camera, renderer, light} = init()
 let controls = initControls(camera, renderer)
 let moveCamera = cameraMover(camera)
 
-let loader = new THREE.GLTFLoader();
 let mixers = []
+let loader = new THREE.GLTFLoader();
 const clock = new THREE.Clock();
 const mouse = new THREE.Vector2();
 window.camera = camera
@@ -20,79 +20,15 @@ wall.mesh.position.y = 10
 camera.position.z = 25
 console.log(scene)
 console.log(wall)
-// navigator.getUserMedia = navigator.getUserMedia ||
-//     navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-let video
-let w = 640
-let h = 480
-navigator.mediaDevices.getUserMedia({audio: false, video: {width: w, height: h}}).then(
-  stream => {
-    video = document.getElementById('video')
-    video.width = w
-    video.height = h
-    video.srcObject = stream
-    video.onloadedmetadata = e => {
-      video.play()
-    }
-  }
-)
-
-
-const imageElement = document.getElementById('img');
-let n
-let argPoseNet = {
-  algorithm: 'Single-pose',
-  input: {
-    architecture: 'MobileNetV1',
-    outputStride: 16,
-    inputResolution: 500,
-    multiplier: 0.75,
-    quantBytes: 2
-  },
-  singlePosedetection: {
-    minPoseConfidence: 0.01,
-    minPartConfidence: 0.9
-  },
-  output: {
-    showVideo: true,
-    showPoints: true
-  }
-}
-posenet.load(argPoseNet).then(value => {
-  console.log(value)
-  n = value
-  start()
-})
-
-let target = 0
-async function estimatePoseOnImage(imageElement) {
-  // load the posenet model from a checkpoint
-  // let n = await posenet.load()
-  const pose = await n.estimateSinglePose(video, {
-    flipHorizontal: true,
-    decodingMethod: 'single-person'
-  });
-  console.log(pose)
-  console.log(pose.keypoints[0].position.x)
-  console.log(pose.keypoints[15].position.x)
-  target = pose.keypoints[0].position.x * 0.1
-  wall.mesh.position.x -= (wall.mesh.position.x - target*0.1)*0.75
-  return pose;
-}
 
 let cpt = 0
 const start = () => {
   renderer.setAnimationLoop( () => {
     update()
     cpt ++
-    if (cpt % 25 === 0) {
-
-      estimatePoseOnImage(imageElement)
-    }
     renderer.render(scene, camera)
   })
 }
-
 
 const update = () => {
   const delta = clock.getDelta()
@@ -100,9 +36,15 @@ const update = () => {
     mixer.update(delta)
   }
   moveCamera()
+  if (cpt % 1 === 0) {
+    estimatePoseOnImage(wall)
+  }
   // controls.update()
   // light.position.x -= 0.01
 }
+
+startCamera()
+loadPoseNet(start)
 
 
 loader.load( 'world.glb', gltf => {
--- poseDetector.js
+++ poseDetector.js
@@ -1,26 +1,84 @@
-// // https://dev.to/devdevcharlie/playing-beat-saber-in-the-browser-with-body-movements-using-posenet-tensorflow-js-36km
-// let net = loadNet()
-//
-// async function estimate(net) {
-//   // image = getimg();
-//   let image = document.getElementById('img');
-//   const poses = await net.estimateSinglePose(image, {
-//     flipHorizontal: true
-//   })
-//   predicted = poses
-//   console.log(predicted)
-// }
-//
-//
-// async function loadNet() {
-//   const net = await posenet.load()
-//   return net
-// }
-//
-// // estimate(net)
-// export {estimate, loadNet}
-//
-// // const net = await posenet.load()
-// // const pose = await net.estimateSinglePose(image, {
-// //   flipHorizontal: false
-// // })
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+let target = 0
+
+let poseNetModel
+let video
+const WIDTH_VIDEO = 640
+const HEIGHT_VIDEO = 480
+
+const startCamera = () => {
+  navigator.mediaDevices.getUserMedia({audio: false, video: {width: WIDTH_VIDEO, height: HEIGHT_VIDEO, frameRate: { max: 60 }}}).then(
+    stream => {
+      video = document.getElementById('video')
+      video.width = WIDTH_VIDEO
+      video.height = HEIGHT_VIDEO
+      video.srcObject = stream
+      video.onloadedmetadata = e => {
+        video.play()
+      }
+    }
+  )
+}
+
+
+
+let argPoseNet = {
+  algorithm: 'Single-pose',
+  input: {
+    architecture: 'ResNet50',
+    outputStride: 16,
+    inputResolution: {width: WIDTH_VIDEO, height: HEIGHT_VIDEO},
+    multiplier: 1.0,
+    quantBytes: 4
+  },
+  singlePosedetection: {
+    minPoseConfidence: 0.01,
+    minPartConfidence: 0.9
+  },
+  output: {
+    showVideo: true,
+    showPoints: true
+  }
+}
+
+const loadPoseNet = callback => {
+  posenet.load(argPoseNet).then(model => {
+    console.log(model)
+    poseNetModel = model
+    callback()
+  })
+}
+
+
+const estimatePoseOnImage = async (wall) => {
+  let pose = await poseNetModel.estimateSinglePose(video, {
+    flipHorizontal: true,
+    decodingMethod: 'single-person'
+  })
+  console.log(pose)
+  // console.log(pose.keypoints[0].position.x)
+  // console.log(pose.keypoints[15].position.x)
+  target = pose.keypoints[0].position.x * 0.1
+  wall.mesh.position.x -= (wall.mesh.position.x - target*0.1)*0.9
+  drawPose(pose.keypoints)
+
+  return pose
+}
+
+let canvas = document.getElementById('output')
+let context = canvas.getContext('2d')
+context.canvas.width = WIDTH_VIDEO
+context.canvas.height = HEIGHT_VIDEO
+const drawPose = keypoints => {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  context.fillStyle = "#00ff00"
+  for (let i = 0; i < 16; i++) {
+    if (keypoints[i].score > 0.3) {
+      context.beginPath()
+      context.arc(keypoints[i].position.x, keypoints[i].position.y, 3, 0, 2*Math.PI)
+      context.fill()
+    }
+  }
+}
+
+export {loadPoseNet, startCamera, estimatePoseOnImage}
